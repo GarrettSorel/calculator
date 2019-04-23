b2d1b2e66c431bfefbae5554853ea39e33b523fe
GarrettSorel <garrettsorel@gmail.com>
2019-04-23 12:42:19 -0400

    front end update - add operate function

--- js/script.js
+++ js/script.js
@@ -1,21 +1,31 @@
-const calculator = document.querySelector(".container");
-
 function add(a, b) {
-  let sum = a + b;
-  return sum;
+  return a + b;
 }
 
 function subtract(a, b) {
-  let sub = a - b;
-  return sub;
+  return a - b;
 }
 
 function multiply(a, b) {
-  let multiply = a * b;
-  return multiply;
+  return a * b;
 }
 
 function divide(a, b) {
-  let div = a / b;
-  return div;
+  if (b === 0) {
+    return "Not Like This...Not Like This..."
+  } else {
+  return a / b;
+}
+
+function operate(op, a, b) {
+  if (op === "+") {
+    add(a,b);
+  } else if (op === "-") {
+    subtract(a,b);
+  } else if (op === "*") {
+    multiply(a,b);
+  } else if (op === "/") {
+    divide(a,b);
+  }
+}
 }
